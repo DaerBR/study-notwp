d87640f11579af416855642607b4d2a30068494c
DaerBR <13daer@gmail.com>
2020-11-02 10:53:53 +0200

    Add back RRD

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,30 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, Component} from 'react';
 import './App.scss';
 import HeaderMenu from './HeaderMenu';
-import Route from "./Route";
 import PostsList from "./PostsList";
 import Post from "./Post";
 import UsersList from "./UsersList";
 import UserProfile from "./UserProfile";
 import CommentsList from "./CommentsList";
+import { Router, Route, Switch } from 'react-router-dom';
 
+import history from "../history";
 
 const App = () => {
     return (
         <div className="app-wrapper">
+            <Router history={history}>
                 <HeaderMenu />
                 <div className="content-wrapper">
-                    <Route path="/">
-                        <PostsList />
-                    </Route>
-                    <Route path="/users">
-                        <UsersList />
-                    </Route>
-                    <Route path="/comments">
-                        <CommentsList />
-                    </Route>
-                    {/*<Route path="/post/:id">*/}
-                    {/*    <UsersList />*/}
-                    {/*</Route>*/}
-                    <Route path='/user/'>
-                        <UserProfile />
-                    </Route>
+                    <Switch>
+                        <Route path="/" exact component={PostsList} />
+                        <Route path="/post/:id" exact component={Post} />
+                        <Route path="/users" exact component={UsersList} />
+                        <Route path="/user/:id" exact component={UserProfile} />
+                        <Route path="/comments" exact component={CommentsList} />
+                    </Switch>
                 </div>
+            </Router>
         </div>
     );
 };
